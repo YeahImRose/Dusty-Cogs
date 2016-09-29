871bf1353b5793642f1935a2e9f72b868271fdfd
Lunar Dust <infinitycreeper1@gmail.com>
2016-09-29 08:38:05 -0500

    Allows user AND server to be toggled
    also may cause a seg fault if you do weird stuff

--- greet/greet.py
+++ greet/greet.py
@@ -17,8 +17,6 @@ except:  # Missing opus
 else:
     opus = True
 
-default_settings = {"ON": False}
-
 class Greet:
     """Plays a sound effect when a user joins a channel"""
 
@@ -31,8 +29,6 @@ class Greet:
 
     async def _join_voice_channel(self, channel):
         server = channel.server
-        if True:
-            njsfnskjdfnsjfnks = 1
         try:
             await self.bot.join_voice_channel(channel)
         except asyncio.futures.TimeoutError as e:
@@ -67,84 +63,111 @@ class Greet:
         except ProcessLookupError:
             pass
 
-        #print("making player on sid {}".format(server.id))
-
         voice_client.audio_player = voice_client.create_ffmpeg_player(song_filename, use_avconv=use_avconv, options=options)
-
-        # Set initial volume
-        vol = 50
-        voice_client.audio_player.volume = vol
-
+        voice_client.audio_player.volume = 50
         return voice_client  # Just for ease of use, it's modified in-place
 
 
     @commands.group(pass_context=True, no_pm=True)
-    @checks.admin_or_permissions(manage_server=True)
     async def greetset(self, ctx):
         """Sets welcome module settings"""
-        server = ctx.message.server
-        if server.id not in self.settings:
-            self.settings[server.id] = default_settings
-            fileIO("data/greet/settings.json","save",self.settings)
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
+        if not ctx.message.author.bot:
+            server = ctx.message.server
+            if str(server.id) not in self.settings:
+                self.settings[server.id] = {"ENABLED": False}
+                fileIO("data/greet/settings.json","save", self.settings)
+            if ctx.invoked_subcommand is None:
+                await send_cmd_help(ctx)
 
     @greetset.command(pass_context=True)
-    async def sound(self, ctx, user: discord.Member, path):
+    async def sound(self, ctx, path, user: discord.Member = None):
         """Set the sound file to play when a specific user joins a voice channel."""
         server = ctx.message.server
+        if user is not None:
+            if str(user.id) not in self.settings[server.id]:
+                self.settings[server.id][user.id] = [False,""]
+        else:
+            user = ctx.message.author
+            if str(user.id) not in self.settings[server.id]:
+                self.settings[server.id][ctx.message.author.id] = [False,""]
         temppath = os.path.join("data/greet/sounds", path)
         temppath = Path(temppath)
-        if not temppath.is_file():
-            self.bot.say("File does not exist! Make sure the file is in the `data/greet/sounds` folder and you are including the file extension.")
-        else:
-            self.settings[server.id][user.id] = str(path)
-            fileIO("data/greet/settings.json", "save", self.settings)
-            await self.bot.say("Set sound effect for a user.")
+        if ctx.message.author == user or checks.mod_or_permissions(mute_members=True):
+            if not temppath.is_file():
+                self.bot.say("File does not exist! Make sure the file is in the `data/greet/sounds` folder and you are including the file extension.")
+            else:
+                if user == None:
+                    self.settings[server.id][ctx.message.author.id][1] = str(path)
+                else:
+                    self.settings[server.id][user.id][1] = str(path)
+                fileIO("data/greet/settings.json", "save", self.settings)
+                await self.bot.say("Set sound effect for " + user.name)
 
+    #This is horrific, sorry
     @greetset.command(pass_context=True)
-    async def toggle(self, ctx):
-        """Turns on/off sounds for a user or a server"""
+    async def toggle(self, ctx, mode: str, user: discord.Member = None):
+        """Turns on/off sounds for a user or a server
+
+        Modes:
+        server:\ttoggle join sounds for the server on/off
+        user:\ttoggle join sounds for a specific user"""
         server = ctx.message.server
-        self.settings[server.id]["ON"] = not self.settings[server.id]["ON"]
-        if self.settings[server.id]["ON"]:
-            await self.bot.say("I will now play channel join sounds.")
+        if user is not None:
+            if str(user.id) not in self.settings[server.id]:
+                self.settings[server.id][user.id] = [False, ""]
         else:
-            await self.bot.say("I will no longer play channel join sounds.")
-        fileIO("data/greet/settings.json", "save", self.settings)
+            user = ctx.message.author
+            if str(user.id) not in self.settings[server.id]:
+                self.settings[server.id][ctx.message.author.id] = [False, ""]
+        if mode is None:
+            return
+        if mode == "server":
+            if checks.mod_or_permissions(mute_members=True):
+                try:
+                    self.settings[server.id]["ENABLED"] = not self.settings[server.id]["ENABLED"]
+                except:
+                    self.settings[server.id]["ENABLED"] = True
+                await self.bot.say("Toggled server-wide join sounds to {0}".format(self.settings[server.id]["ENABLED"]))
+                fileIO("data/greet/settings.json", "save", self.settings)
+        if mode == "user":
+            if user == None:
+                await self.bot.say("Please specifiy a user.")
+                return
+            if ctx.message.author == user or checks.mod_or_permissions(mute_members=True):
+                try:
+                    self.settings[server.id][user.id][0] = not self.settings[server.id][user.id][0]
+                except:
+                    self.settings[server.id][user.id][0] = True
+                await self.bot.say("Toggled sound for {0.name} to {1}.".format(user, self.settings[server.id][user.id][0]))
+                fileIO("data/greet/settings.json", "save", self.settings)
 
     async def user_join(self, before, after):
+        audio = self.bot.get_cog('Audio')
         channel = after.voice_channel
         server = after.server
+        bot_chan = self.bot.voice_client_in(server)
         if after is None:
             return
+        if before.voice_channel == after.voice_channel:
+            return
+
         if after.id == self.bot.user.id:
             return
-        if server.id not in self.settings:
-            pass
-            self.settings[server.id] = default_settings
-            fileIO("data/greet/settings.json","save",self.settings)
-        if not self.settings[server.id]["ON"]:
+        if str(server.id) not in self.settings:
             return
-        if server == None:
-            print("AAAAAAAAAAAAAAAAAAAAAAAAAAA! Something went wrong.")
+        if not self.settings[server.id]["ENABLED"]:
             return
-        if channel is None:
-            print('Voice channel not found... somehow.')
+        if not self.settings[server.id][after.id][0]:
             return
-
-        voice_client = await self._create_ffmpeg_player(server, str(self.settings[server.id][after.id]), local=True)
-        voice_client.audio_player.start()
-        #except:
-        #    print("Permissions Error. User that joined a voice channel: {0.name}".format(member))
-        #    print("Bot doesn't have the proper permissions for {1.name} channel".format(channel))
-
-
-    def speak_permissions(self, member):
-        channel = self.member.voice.voice_channel
-        if channel is None:
-            return False
-        return server.get_member(self.bot.user.id).permissions_in(channel).speak
+        if audio.is_playing(server):
+            return
+        try:
+            #    await audio._stop_and_disconnect(server)
+            #    await self._join_voice_channel(channel)
+            voice_client = await self._create_ffmpeg_player(server, str(self.settings[server.id][after.id][1]), local=True)
+            voice_client.audio_player.start()
+        except:
+            print("Something went very wrong...")
 
 
 def check_folders():
