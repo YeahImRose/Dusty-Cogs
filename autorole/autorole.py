cbddb239a11d56420d936d715d1f80dbf6d4085a
Lunar Dust <infinitycreeper1@gmail.com>
2017-01-26 13:25:22 -0600

    ToS added to autorole/new cog

--- autorole/autorole.py
+++ autorole/autorole.py
@@ -1,12 +1,18 @@
 import discord
 from discord.ext import commands
 from cogs.utils import checks
-from __main__ import set_cog, send_cmd_help, settings
+from __main__ import send_cmd_help
 from .utils.dataIO import dataIO
 from .utils.chat_formatting import *
-import aiohttp
-import asyncio
+import random
+import string
 import os
+
+
+class PermissionsError:
+    print("Bot probably doesn't have permissions to manage roles."
+          "Check your discord server permissions.\n")
+
 
 class Autorole:
     """Autorole commands."""
@@ -15,38 +21,90 @@ class Autorole:
         self.bot = bot
         self.file_path = "data/autorole/settings.json"
         self.settings = dataIO.load_json(self.file_path)
+        self.users = {}
+        self.messages = {}
 
     def _get_server_from_id(self, serverid):
         return discord.utils.get(self.bot.servers, id=serverid)
 
-    async def _give_role(self, member):
-        server = member.server
-        
-        if server.id not in self.settings:
-            self.settings[server.id] = {}
-            self.settings[server.id]["ENABLED"] = False
-            self.settings[server.id]["ROLE"] = None
-            dataIO.save_json(self.file_path, self.settings)
-            
-        if self.settings[server.id]["ENABLED"] is True:
+    def _set_default(self, server):
+        self.settings[server.id] = {}
+        self.settings[server.id]["ENABLED"] = False
+        self.settings[server.id]["ROLE"] = None
+        self.settings[server.id]["AGREE_CHANNEL"] = None
+        dataIO.save_json(self.file_path, self.settings)
+
+    async def on_message(self, message):
+        server = message.server
+        user = message.author
+        try:
+            if server.id not in self.settings:
+                self._set_default(server)
+
+            ch = discord.utils.get(
+                self.bot.get_all_channels(),
+                id=self.settings[server.id]["AGREE_CHANNEL"])
+
+            if message.channel != ch:
+                return
+        except:
+            return
+
+        if message.content == self.users[user.id]:
             roleid = self.settings[server.id]["ROLE"]
             try:
                 roles = server.roles
             except AttributeError:
-                server = self._get_server_from_id(server)
-                try:
-                    roles = server.roles
-                except AttributeError:
-                    raise RoleNotFound(server, roleid)
+                print("Server roles not found, what did you even do?\n")
+                return
 
             role = discord.utils.get(roles, id=roleid)
             try:
-                await self.bot.add_roles(member, role)
+                await self.bot.add_roles(user, role)
+                await self.bot.delete_message(message)
+                if user.id in self.messages:
+                    self.messages.pop(user.id, None)
 
             except discord.Forbidden:
-                print("An error has occured. User that joined: {0.name}".format(member))
-                print("Bot probably doesn't have permissions to manage roles. Check your discord server permissions.")
-            #    print("Or you manually edited the JSON...")
+                raise PermissionError
+
+    async def _give_role(self, member):
+        server = member.server
+        if server.id not in self.settings:
+            self._set_default(server)
+
+        if self.settings[server.id]["ENABLED"] is True:
+            if self.settings[server.id]["AGREE_CHANNEL"] is not None:
+                # <3 Stackoverflow http://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits-in-python/23728630#23728630
+                key = ''.join(random.choice(string.ascii_uppercase +
+                                            string.digits) for _ in range(6))
+                self.users[member.id] = key
+
+                ch = discord.utils.get(
+                    self.bot.get_all_channels(),
+                    id=self.settings[server.id]["AGREE_CHANNEL"])
+
+                _temp_msg = ("{.name}, please enter {} "
+                             "to accept these rules".format(member, key))
+                try:
+                    await self.bot.send_message(member, _temp_msg)
+                except:
+                    msg = await self.bot.send_message(ch, _temp_msg)
+                    self.messages[member.id] = msg
+            else:
+                roleid = self.settings[server.id]["ROLE"]
+                try:
+                    roles = server.roles
+                except AttributeError:
+                    print("Server roles not found, what did you even do?\n")
+                    return
+
+                role = discord.utils.get(roles, id=roleid)
+                try:
+                    await self.bot.add_roles(member, role)
+
+                except discord.Forbidden:
+                    raise PermissionError
 
     @commands.group(name="autorole", pass_context=True, no_pm=True)
     async def autorole(self, ctx):
@@ -58,11 +116,13 @@ class Autorole:
             self.settings[server.id] = {}
             self.settings[server.id]["ENABLED"] = False
             self.settings[server.id]["ROLE"] = None
+            self.settings[server.id]["AGREE_CHANNEL"] = None
             dataIO.save_json(self.file_path, self.settings)
 
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
-            await self.bot.say("```Current autorole state: " + str(self.settings[server.id]["ENABLED"]) + " ```")
+            await self.bot.say("```Current autorole state: {}```".format(
+                self.settings[server.id]["ENABLED"]))
 
     @autorole.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(manage_roles=True)
@@ -70,7 +130,9 @@ class Autorole:
         """Enables/Disables autorole"""
         server = ctx.message.server
         if self.settings[server.id]["ROLE"] is None:
-            await self.bot.say("You haven't set a role to assign to new users! Use `" + ctx.prefix + "autorole role \"role\"` to set it!")
+            await self.bot.say("You haven't set a role to give to new users! "
+                               "Use `{}autorole role \"role\"` to set it!"
+                               .format(ctx.prefix))
         else:
             if self.settings[server.id]["ENABLED"] is True:
                 self.settings[server.id]["ENABLED"] = False
@@ -84,18 +146,39 @@ class Autorole:
     @autorole.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(manage_roles=True)
     async def role(self, ctx, role: discord.Role):
-        """Set role for autorole to assign. 
-        
+        """Set role for autorole to assign.
+
         Use quotation marks around the role if it contains spaces."""
         server = ctx.message.server
         self.settings[server.id]["ROLE"] = role.id
         await self.bot.say("Autorole set to " + role.name)
         dataIO.save_json(self.file_path, self.settings)
 
+    @autorole.command(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    async def channel(self, ctx, channel: str):
+        """Set the channel that will be used for accepting the rules.
+
+        Entering only \"#\" will disable this."""
+        server = ctx.message.server
+        if channel.startswith("#"):
+            channel = channel[1:]
+        if channel == "":
+            self.settings[server.id]["AGREE_CHANNEL"] = None
+            await self.bot.say("Agreement channel cleared")
+        for x in ctx.message.server.channels:
+            if x.name == channel:
+                self.settings[server.id]["AGREE_CHANNEL"] = x.id
+                await self.bot.say("Agreement channel "
+                                   "set to {}".format(x.name))
+        dataIO.save_json(self.file_path, self.settings)
+
+
 def check_folders():
     if not os.path.exists("data/autorole"):
         print("Creating data/autorole folder...")
         os.makedirs("data/autorole")
+
 
 def check_files():
 
@@ -103,6 +186,7 @@ def check_files():
     if not dataIO.is_valid_json(f):
         print("Creating default autorole's settings.json...")
         dataIO.save_json(f, {})
+
 
 def setup(bot):
     check_folders()
---
+++ requester/requester.py
@@ -0,0 +1,121 @@
+import discord
+import os
+from cogs.utils.dataIO import dataIO
+from discord.ext import commands
+from .utils.chat_formatting import *
+from cogs.utils import checks
+import asyncio
+
+
+class Requester:
+    def __init__(self, bot):
+        self.bot = bot
+        self.path = "data/requester/settings.json"
+        self.settings = dataIO.load_json(self.path)
+
+    @commands.command(name="request", pass_context=True, no_pm=True)
+    async def _request(self, ctx, role: str):
+        """Gain the role of the requested role if it is included in the role list."""
+        user = ctx.message.author
+        server = ctx.message.server
+        add = None
+        if not self.settings[server.id]["ENABLED"]:
+            return
+
+        if role.lower() in self.settings[server.id]["ROLES"]:
+            r = [x for x in server.roles if x.name.lower() == role.lower()][0]
+            if r:
+                add = r
+
+        if add:
+            await self.bot.add_roles(user, add)
+
+    @commands.group(pass_context=True)
+    async def rset(self, ctx):
+        """Change various settings for Requester"""
+        if ctx.invoked_subcommand is None:
+            return
+
+    @rset.command(no_pm=True, pass_context=True)
+    @checks.admin_or_permissions(manage_roles=True)
+    async def toggle(self, ctx, state: str):
+        """Changes or displays the state of requesting roles"""
+        trues = ["true", "on", "enabled", "enable", "1"]
+        falses = ["false", "off", "disabled", "disable", "0"]
+        server = ctx.message.server
+
+        try:
+            s = self.settings[server.id]["ENABLED"]
+        except KeyError:
+            if self.settings[server.id] is None:
+                self.settings[server.id] = {}
+            else:
+                s = False
+
+        if state.lower() in trues:
+            s = True
+        elif state.lower() in falses:
+            s = False
+        else:
+            return await self.bot.say("Current state: {}".format(str(s).lower()))
+
+        self.settings[server.id]["ENABLED"] = bool(s)
+        dataIO.save_json(self.path, self.settings)
+        await self.bot.say("State set toggle {}".format(str(s).lower()))
+
+    @rset.command(no_pm=True, pass_context=True)
+    @checks.mod_or_permissions(manage_roles=True)
+    async def addrole(self, ctx, role: str):
+        """Add a role to the list of requestable roles"""
+        server = ctx.message.server
+        try:
+            roles = self.settings[server.id]["ROLES"]
+        except KeyError:
+            self.settings[server.id] = {}
+            roles = []
+        if roles:
+            if [x for x in server.roles if x.name == role]:
+                roles.append(role.lower())
+        else:
+            roles = [role]
+
+        self.settings[server.id]["ROLES"] = roles
+        dataIO.save_json(self.path, self.settings)
+        await self.bot.say("Role {} added.".format(role))
+
+    @rset.command(no_pm=True, pass_context=True)
+    @checks.mod_or_permissions(manage_roles=True)
+    async def delrole(self, ctx, role: str):
+        """Remove a role from the list of requestable roles"""
+        server = ctx.message.server
+        try:
+            roles = self.settings[server.id]["ROLES"]
+        except KeyError:
+            self.settings[server.id] = {}
+            roles = []
+        if roles:
+            if role in roles:
+                roles.remove(role.lower())
+
+        self.settings[server.id]["ROLES"] = roles
+        dataIO.save_json(self.path, self.settings)
+        await self.bot.say("Role {} removed.".format(role))
+
+
+def check_folders():
+    if not os.path.exists("data/requester"):
+        print("Creating data/requester folder...")
+        os.makedirs("data/requester")
+
+
+def check_files():
+    f = "data/requester/settings.json"
+    if not dataIO.is_valid_json(f):
+        print("Creating default requester's settings.json...")
+        dataIO.save_json(f, {})
+
+
+def setup(bot):
+    check_folders()
+    check_files()
+    bot.add_cog(Requester(bot))
